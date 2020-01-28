03dc082c4869ee1a4b6a64c884d2185db3e4d1ac
Curtis pachtinger <cpach96@gmail.com>
2020-01-28 16:14:11 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,12 +1,19 @@
 def using_include(array, element)
-
+array.include?(element)
 end
 
 def using_sort(array)
-
+  
+  array_test = ["I" , "jump" , "wow"]
+  
+  array_test.sort
 end
 
 def using_reverse(array)
+
+  array_test = ["wow" , "beans" , "arrays!"]
+  
+  array_test.reverse
 
 end
 
